d0e642f043d0fe8218a90b69e7780b2b9a69a1b6
Adrielrusli <Adrielbernard28@yahoo.com>
2023-05-04 01:07:53 +0700

    Initial Commit

---
+++ TestWach Watch App/TestWachApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWachApp.swift
+//  TestWach Watch App
+//
+//  Created by Adriel Bernard Rusli on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TestWach_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestWach/TestWachApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWachApp.swift
+//  TestWach
+//
+//  Created by Adriel Bernard Rusli on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TestWachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
